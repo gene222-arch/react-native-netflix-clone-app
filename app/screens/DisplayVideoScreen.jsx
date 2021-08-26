677350e437d34d3a4d15ff2bcc37d24b741ef648
gene222-arch <genephillip222@gmail.com>
2021-08-26 18:34:29 +0800

    chore: change useEffect to useFocusEffect lifecycle

--- app/screens/DisplayVideoScreen.jsx
+++ app/screens/DisplayVideoScreen.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { InteractionManager } from 'react-native'
 import VideoPlayerFullScreen from '../components/VideoPlayerFullScreen';
-import { useNavigation, useFocusEffect, useRoute } from '@react-navigation/native';
+import { useFocusEffect, useRoute } from '@react-navigation/native';
 import LoadingSpinner from '../components/LoadingSpinner';
 import * as FileSystem from 'expo-file-system';
 import { ensureFileExists } from './../utils/cacheImage';
@@ -9,40 +9,41 @@ import { getExtension } from './../utils/file';
 
 const DisplayVideoScreen = () => 
 {
-    const navigation = useNavigation();
     const route = useRoute();
     
     const [ uri, setUri ] = useState(null);
     const [ shouldPlay, setShouldPlay ] = useState(false);
     const [ isInteractionsComplete, setIsInteractionsComplete ] = useState(false);
 
-    useEffect(() => {
-        InteractionManager.runAfterInteractions(async () => 
-        {
-            try {   
-                const { id, title, videoUri } = route.params;
-                const fileExt = getExtension(videoUri);
-                
-                const fileToCacheUri = FileSystem.cacheDirectory + `${ id }${ title }` + `.${ fileExt }`;
-                const { exists } = await ensureFileExists(fileToCacheUri);
-        
-                if (! exists) {
-                    await FileSystem.downloadAsync(videoUri, fileToCacheUri)
-                }
+    useFocusEffect(
+        useCallback(() => {
+            InteractionManager.runAfterInteractions(async () => 
+            {
+                try {   
+                    const { id, title, videoUri } = route.params;
+                    const fileExt = getExtension(videoUri);
+                    
+                    const fileToCacheUri = FileSystem.cacheDirectory + `${ id }${ title }` + `.${ fileExt }`;
+                    const { exists } = await ensureFileExists(fileToCacheUri);
             
-                setUri(fileToCacheUri);
-                setShouldPlay(true);
-            } catch ({ message }) {
-                console.log(message);
+                    if (! exists) {
+                        await FileSystem.downloadAsync(videoUri, fileToCacheUri)
+                    }
+                
+                    setUri(fileToCacheUri);
+                    setShouldPlay(true);
+                } catch ({ message }) {
+                    console.log(message);
+                }
+    
+                setIsInteractionsComplete(true);
+            });
+    
+            return () => {
+                setUri(null);
             }
-
-            setIsInteractionsComplete(true);
-        });
-
-        return () => {
-            setUri(null);
-        }
-    }, [route.params])
+        }, [route.params])
+    )
 
     if (! isInteractionsComplete) return <LoadingSpinner message='Loading' />
 
