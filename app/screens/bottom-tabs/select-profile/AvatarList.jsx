419d2295a008df1c73ec9809b1ae8ec618a35d08
gene222-arch <genephillip222@gmail.com>
2021-10-19 21:08:43 +0800

    style: border width

--- app/screens/bottom-tabs/select-profile/AvatarList.jsx
+++ app/screens/bottom-tabs/select-profile/AvatarList.jsx
@@ -245,7 +245,7 @@ const styles = StyleSheet.create({
     },
     takeAPhotoBtn: {
         backgroundColor: 'transparent',
-        borderWidth: 1,
+        borderWidth: 1.5,
         borderColor: Colors.white
     },
     uploadImgContainer: {
