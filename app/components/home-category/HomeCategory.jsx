9ceec7b5bc298e358ee825b1ab80601bd0f054cc
gene222-arch <genephillip222@gmail.com>
2021-10-21 02:13:05 +0800

    feat: separate display movie component

--- app/components/home-category/HomeCategory.jsx
+++ app/components/home-category/HomeCategory.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import { Image } from "react-native-expo-image-cache";
 import { useNavigation } from '@react-navigation/native'
-import { FlatList, Pressable } from 'react-native'
+import { FlatList, TouchableOpacity } from 'react-native'
 import styles from '../../assets/stylesheets/homeCategory';
 import Text from '../Text';
 import View from './../View';
 import MostLikedBadge from './../MostLikedBadge';
 
+const DisplayMovieImage = ({ item, onPressImage }) => 
+{
+    return (
+        <TouchableOpacity 
+            onPress={ onPressImage } 
+            style={ styles.imageContainer }
+        >
+            <Image 
+                preview={{ uri: item.poster_path }}
+                style={ styles.image }
+                uri={ item.poster_path }
+            />
+            <MostLikedBadge movieId={ item.id } />
+        </TouchableOpacity>
+    )
+}
 
 const HomeCategory = ({ title, categorizedMovies }) => 
 {
@@ -19,29 +35,22 @@ const HomeCategory = ({ title, categorizedMovies }) =>
         });
     }
 
-    return categorizedMovies.length > 0 && (
-        <View>
-            <Text h4 style={ styles.categoryTitle }>{ title }</Text>
-            <FlatList 
-                keyExtractor={(item, index) => index.toString() }
-                data={ categorizedMovies }
-                renderItem={({ item }) => (
-                    <Pressable onPress={ () => handlePressImage(item) } style={ styles.imageContainer }>
-                        <Image 
-                            preview={{ uri: item.poster_path }}
-                            style={ styles.image }
-                            uri={ item.poster_path }
-                        />
-                        <MostLikedBadge movieId={ item.id } />
-                    </Pressable>
-                )}
-                maxToRenderPerBatch={ 3 }
-                horizontal
-                style={ styles.categoryContainer }
-                showsHorizontalScrollIndicator={ false }
-                showsVerticalScrollIndicator={ false }
-            />
-        </View>
+    return (
+        Boolean(categorizedMovies.length) && (
+            <View>
+                <Text h4 style={ styles.categoryTitle }>{ title }</Text>
+                <FlatList 
+                    keyExtractor={(item, index) => index.toString() }
+                    data={ categorizedMovies }
+                    renderItem={({ item }) => <DisplayMovieImage item={ item } onPressImage={ () => handlePressImage(item) } /> }
+                    maxToRenderPerBatch={ 3 }
+                    horizontal
+                    style={ styles.categoryContainer }
+                    showsHorizontalScrollIndicator={ false }
+                    showsVerticalScrollIndicator={ false }
+                />
+            </View>
+        )
     )
 }
 
