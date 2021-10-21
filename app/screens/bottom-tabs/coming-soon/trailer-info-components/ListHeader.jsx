c65515b8105f676d4e27bc35dc02689a94e91bfa
gene222-arch <genephillip222@gmail.com>
2021-10-22 05:17:52 +0800

    refactor: remove unused imports and variables

--- app/screens/bottom-tabs/coming-soon/trailer-info-components/ListHeader.jsx
+++ app/screens/bottom-tabs/coming-soon/trailer-info-components/ListHeader.jsx
@@ -7,16 +7,12 @@ import ActionButton from '../../../../components/ActionButton';
 import TrailerAndMoreLikeThisTab from './TrailerAndMoreLikeThisTab';
 import MoreLikeThis from './MoreLikeThis';
 import TrailersAndMore from './TrailersAndMore';
-import { useDispatch, connect } from 'react-redux';
-import * as AUTH_ACTION from './../.././../../redux/modules/auth/actions'
 import { Divider } from 'react-native-elements';
 import styles from './../../../../assets/stylesheets/trailerInfo';
-import { createStructuredSelector } from 'reselect';
-import { authProfileSelector } from './../../../../redux/modules/auth/selectors';
 
-const ListHeader = ({ AUTH_PROFILE, comingSoonMovie }) => 
+
+const ListHeader = ({ comingSoonMovie }) => 
 {
-    const hasLikedMovie = Boolean(AUTH_PROFILE.liked_coming_soon_movies.find(({ movie_id }) => movie_id === comingSoonMovie.id));
     const [ selectedTabCategory, setSelectedTabCategory ] = useState(1);
 
     const handlePressSimilarShow = (recommendedSimilarShow) => navigation.push('TrailerInfo', { comingSoonMovie: recommendedSimilarShow });
@@ -60,8 +56,4 @@ const ListHeader = ({ AUTH_PROFILE, comingSoonMovie }) =>
     )
 }
 
-const mapStateToProps = createStructuredSelector({
-    AUTH_PROFILE: authProfileSelector
-});
-
-export default connect(mapStateToProps)(ListHeader)
+export default ListHeader
