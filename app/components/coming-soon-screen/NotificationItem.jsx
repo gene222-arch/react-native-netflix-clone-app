8b5c09ba39d60525a59eadc382c6a858d6ac2cb0
gene222-arch <genephillip222@gmail.com>
2021-10-21 23:22:34 +0800

    feat: added api integration for NotificationsScreen

--- app/components/coming-soon-screen/NotificationItem.jsx
+++ app/components/coming-soon-screen/NotificationItem.jsx
@@ -6,26 +6,24 @@ import { Image } from 'react-native-expo-image-cache';
 import Colors from './../../constants/Colors';
 import { useNavigation } from '@react-navigation/native';
 
-const NotificationItem = ({ id = 1 }) => 
+const NotificationItem = ({ item }) => 
 {
     const navigation = useNavigation();
 
-    const handlePress = () => navigation.navigate('TrailerInfo', { id });
+    const handlePress = () => navigation.navigate('TrailerInfo', { id: item.movie_id });
 
     return (
         <TouchableOpacity>
             <View style={ styles.container }>
                 <Image 
-                    uri='https://laravel-flicklify-files.s3.ap-southeast-1.amazonaws.com/movies/wallpapers/After-Masks-2021-Cover-1633353211.jpg'
-                    preview={{ 
-                        uri: 'https://laravel-flicklify-files.s3.ap-southeast-1.amazonaws.com/movies/wallpapers/After-Masks-2021-Cover-1633353211.jpg' 
-                    }}
+                    uri={ item.movie.wallpaper_path }
+                    preview={{  uri: item.movie.wallpaper_path  }}
                     style={ styles.img }
                 />
                 <View style={ styles.movieDescriptionContainer }>
-                    <Text style={ styles.movieNotifTypeText }>New Arrival</Text>
-                    <Text style={ styles.releasedAtText }>Movie Title</Text>
-                    <Text style={ styles.releasedAtText }>April 20</Text>
+                    <Text style={ styles.movieNotifTypeText }>{ item.type }</Text>
+                    <Text style={ styles.releasedAtText }>{ item.movie.title }</Text>
+                    <Text style={ styles.releasedAtText }>{ item.created_at }</Text>
                 </View>
             </View>
         </TouchableOpacity>
--- app/screens/bottom-tabs/coming-soon/NotificationsScreen.jsx
+++ app/screens/bottom-tabs/coming-soon/NotificationsScreen.jsx
@@ -1,16 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import View from './../../../components/View';
-import { StyleSheet, FlatList } from 'react-native'
+import { StyleSheet, FlatList, InteractionManager } from 'react-native'
 import NotificationItem from '../../../components/coming-soon-screen/NotificationItem';
+import * as MOVIE_NOTIFICATION_API from './../../../services/movie/movie.notification'
+import { createStructuredSelector } from 'reselect';
+import { movieSelector } from './../../../redux/modules/movie/selectors';
+import { connect } from 'react-redux';
 
-const NotificationsScreen = () => 
+const NotificationsScreen = ({ MOVIE }) => 
 {
+    const [ isInteractionsComplete, setIsInteractionsComplete ] = useState(false);
+    const [ notifications, setNotifications ] = useState([]);
+
+    const onLoadFetchMovieNotifications = async () => 
+    {
+        try {
+            const result = await MOVIE_NOTIFICATION_API.fetchAllAsync();
+
+            if (result) {
+                setNotifications(result.data);
+            }
+        } catch ({ message }) {
+            console.log(message);
+        }
+    }
+
+    useEffect(() => 
+    {
+        InteractionManager.runAfterInteractions(() => {
+            onLoadFetchMovieNotifications();
+            setIsInteractionsComplete(true);
+        });    
+    }, [MOVIE.movies]);
+
     return (
         <View style={ styles.container }>
             <FlatList 
                 keyExtractor={ (item, index) => index.toString() }
-                data={[0, 1, 2, 2, 2, 2, 2]}
-                renderItem={ ({ item }) => <NotificationItem /> }
+                data={ notifications }
+                renderItem={ ({ item }) => <NotificationItem item={ item } /> }
                 showsHorizontalScrollIndicator={ false }
                 showsVerticalScrollIndicator={ false }
             />
@@ -18,7 +46,11 @@ const NotificationsScreen = () =>
     )
 }
 
-export default NotificationsScreen
+const mapStateToProps = createStructuredSelector({
+    MOVIE: movieSelector
+});
+
+export default connect(mapStateToProps)(NotificationsScreen)
 
 const styles = StyleSheet.create({
     container: {
