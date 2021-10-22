e8022d1cb8501d0123b3ea7ff361fc1b6948b5e6
gene222-arch <genephillip222@gmail.com>
2021-10-23 02:35:38 +0800

    feat: add conditional font color

--- app/components/MaturityRatingBadge.jsx
+++ app/components/MaturityRatingBadge.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import { Badge } from 'react-native-elements';
+import Colors from './../constants/Colors';
 
 const MaturityRatingBadge = ({ ageRestriction }) => 
 {
     let status = 'success';
+    let color = Colors.white;
 
     if (ageRestriction >= 13 && ageRestriction < 16) {
         status = 'warning';
+        color = Colors.dark;
     }
 
     if (ageRestriction >= 18) {
         status = 'error';
     }
 
-    return <Badge status={ status } value={ `${ ageRestriction }+` } />
+    return <Badge status={ status } value={ `${ ageRestriction }+` } badgeStyle={{ color }} />
 }
 
 export default MaturityRatingBadge
